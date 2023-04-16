dca9c8c0e34d25a1c4acfd58980d8ebd1e79810e
Chewy <95539361+chewyxrd@users.noreply.github.com>
2023-04-16 18:48:13 +0300

    Create Setup.js

---
+++ source/Commands/Ticket/Setup.js
@@ -0,0 +1,48 @@
+const {MessageActionRow, MessageSelectMenu} = require('discord.js')
+
+module.exports = {
+	config: {
+		aliases: ["setup"],
+		name: "setup",
+        help: "setup",
+		enabled: true
+	},
+
+	run: async ({ client, message, args, embed, guild, author }) => {
+
+        message.delete()
+        const row = new MessageActionRow()
+			.addComponents(
+				new MessageSelectMenu()
+					.setCustomId('select')
+					.setPlaceholder('Oluşturulacak bilet türünü seçin.')
+					.addOptions([
+						{
+							label: 'Yetkili Alım',
+							description: 'Yetkili alım bileti açarsınız.',
+							value: 'yetkilialım',
+						},
+						{
+							label: 'Öneri/İstek/Şikayet',
+							description: 'Öneri/İstek/Şikayet bileti açarsınız.',
+							value: 'öneriistekşikayet',
+						},
+                        {
+							label: 'Destek',
+							description: 'Destek bileti açarsınız.',
+							value: 'destek',
+						},
+					]),
+			);
+
+        message.channel.send({
+            embeds: [{
+                title: 'Bilet',
+                description: 'Lütfen açmak istediğiniz bilet türünü seçin.',
+                color: "BLURPLE",
+                footer: {text: 'Chewy 💙'}
+            }],
+            components: [row]
+        })
+    }
+}
